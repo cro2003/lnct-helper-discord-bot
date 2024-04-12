3dff9cebf53a1041baf0fe8f4035dd43acfeb434
cro2003 <chiragrai2003@icloud.com>
2024-04-12 21:05:50 +0530

    added  command

---
+++ main.py
@@ -0,0 +1,54 @@
+import discord
+import rgpvApi
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.environ.get('TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+async def getResult(enrollId, semester, message):
+    result = rgpvApi.result(enrollId, 24)
+    data = json.loads(result.getMain(semester))
+    if data.get('error'):
+        await message.edit(content=data['error'])
+        return
+    content = f'Student Name: {data["name"]}\n'\
+              f'Enrollment Number: {data["enrollId"]}\n'\
+              f'Status: {data["status"]}\n'\
+              f'SGPA: {data["sgpa"]}\n'\
+              f'CGPA: {data["cgpa"]}\n'
+    for subject in data['subjects']:
+        content += f'{subject["subject"]} - {subject["grade"]}\n'
+    await message.edit(content=content)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$result'):
+        try:
+            enrollId = str(message.content.split(' ')[1])
+            semester = int(message.content.split(' ')[2])
+        except IndexError:
+            await message.channel.send('Invalid Command! \nUsage: $result <enrollId> <semester>')
+            return
+        msg = await message.channel.send('Fetching Result...')
+        await getResult(enrollId, semester, msg)
+        return
+
+    elif message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+        return
+
+client.run(TOKEN)
