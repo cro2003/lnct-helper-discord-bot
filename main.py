f2631f62ff4f2b527865066ebd3c59c1018c2e8c
cro2003 <chiragrai2003@icloud.com>
2024-04-14 13:07:48 +0530

    Added Profile Feature of LNCT Accsoft, it will help users to know about the Profile Information through Accsoft.

--- main.py
+++ main.py
@@ -50,12 +50,25 @@ async def on_message(message):
     elif message.content.startswith('$profile'):
         try:
             username = str(message.content.split(' ')[1])
-            password = int(message.content.split(' ')[2])
+            password = str(message.content.split(' ')[2])
         except IndexError:
-            await message.channel.send('Invalid Command! \nUsage: $result <username> <password>')
+            await message.channel.send('Invalid Command! \nUsage: $profile <username> <password>')
             return
-        accsoft = lnctApi.accsoft()
-
+        accsoft = lnctApi.accsoft(username, password)
+        data = json.loads(accsoft.profile())
+        if data.get('error'):
+            await message.channel.send(content=data['error'])
+            return
+        content = f'Name: {data["name"]}\n' \
+                  f'Cource: {data["course"]}\n'\
+                  f'Section: {data["section"]}\n'\
+                  f'Scholar Id: {data["scholarId"]}\n'\
+                  f'Accsoft Id: {data["enrollmentId"]}\n'\
+                  f'College: {data["college"]}\n'\
+                  f'Mobile Number: {data["MobileNumber"]}\n'\
+                  f'Email: {data["email"]}\n'\
+                  f'Profile Image: [Link]({data["profileImage"]})\n'
+        await message.channel.send(content)
     elif message.content.startswith('$hello'):
         await message.channel.send('Hello!')
         return
